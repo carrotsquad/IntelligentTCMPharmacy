55e76ef50bc67b7fe714629bcea9652c370553a2
heator <heator@outlook.com>
2018-10-21 01:01:19 +0800

    Initial commit

---
+++ app/src/main/java/com/zhangqianyuan/teamwork/intelligenttcmpharmacy/util/AllUri.java
@@ -0,0 +1,10 @@
+package com.zhangqianyuan.teamwork.intelligenttcmpharmacy.util;
+
+/**
+ * Description: 这个类是用来承装所有网络url和apiKey等的
+ * Created at: 2018/10/20 23:33
+ * @author: zhangqianyuan
+ * Email: zhang.qianyuan@foxmail.com
+ */
+public class AllUri {
+}
---
+++ app/src/main/java/com/zhangqianyuan/teamwork/intelligenttcmpharmacy/view/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zhangqianyuan.teamwork.intelligenttcmpharmacy.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhangqianyuan.teamwork.intelligenttcmpharmacy.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
