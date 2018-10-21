92e0c54fbf7ce0be7428d0e96bce30136c7f8460
heator <heator@outlook.com>
2018-10-21 10:50:52 +0800

    重新改了一下文件夹

--- app/src/main/java/com/zhangqianyuan/teamwork/intelligenttcmpharmacy/view/activity/MainActivity.java
+++ app/src/main/java/com/zhangqianyuan/teamwork/intelligenttcmpharmacy/view/activity/MainActivity.java
@@ -1,17 +1,22 @@
-package com.zhangqianyuan.teamwork.intelligenttcmpharmacy.view;
+package com.zhangqianyuan.teamwork.intelligenttcmpharmacy.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.zhangqianyuan.teamwork.intelligenttcmpharmacy.R;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String titles[] = new String[]{"主页", "药材百科", "个人中心"};
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_view);
+        ButterKnife.bind(this);
     }
 }
